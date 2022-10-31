54ca1adad7841d7fbe26ff1ebd234bdd6b31da6b
jieun <sjy990302@naver.com>
2022-10-31 17:04:41 +0900

    221031

--- src/components/Ntweet.js
+++ src/components/Ntweet.js
@@ -1,24 +1,38 @@
 import { async } from "@firebase/util";
 import { dbService } from "fbase";
-import { doc, deleteDoc, updateDoc }from"firebase/firestore";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
+import { deleteObject, ref } from "@firebase/storage";
+import { storageService } from "../fbase";
 
 const Ntweet = ({ ntweetObj, isOwner }) => {
     const [editing, setEditing] = useState(false);
     const [newNtweet, setNewNtweet] = useState(ntweetObj.text);
     const NweetTextRef =doc(dbService, "nweets", `${ntweetObj.id}`);
+    const desertRef = ref(storageService, ntweetObj.attachmentUrl);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delet this ntweet?");
         if (ok) {
-            await deleteDoc(NweetTextRef);
+            try {
+                await deleteDoc(NweetTextRef);
+                if (ntweetObj.attachmentUrl !== "") {
+                    await deleteObject(desertRef);
+                }
+            } catch (error) {
+                window.alert("트윗을 삭제하는 데 실패했습니다!");
+            }
         }
     };
     const toggleEditing = () => setEditing(prev => !prev);
     const onSubmit = async (event) => {
         event.preventDefault();
-        await updateDoc(NweetTextRef, { 
-        text: newNtweet});
-        // setEditing(false);
+        await updateDoc(
+            NweetTextRef, 
+            { 
+                text: newNtweet
+            }
+        );
+        setEditing(false);
     };
     const onChange =(event) => {
         const {
@@ -45,6 +59,9 @@ const Ntweet = ({ ntweetObj, isOwner }) => {
                 ) : (
                 <>
                     <h4>{ntweetObj.text}</h4>
+                    {ntweetObj.attachmentUrl && (
+                        <img src={ntweetObj.attachmentUrl} width="50px" height="50px" />
+                    )}
                     {isOwner && 
                         (
                         <>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import Ntweet from "components/Ntweet";
-import { dbService } from "fbase";
+import { v4 } from "uuid";
+import { dbService, storageService } from "fbase";
 import {
     addDoc,
     collection,
@@ -8,10 +9,12 @@ import {
     orderBy,
     } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
 
-const Home = ({ userObj  }) => {
+const Home = ({ userObj }) => {
     const [ntweet, setNtweet] = useState("");
     const [ntweets, setNtweets] = useState([]);
+    const [attachment, setAttachment] = useState("");
     useEffect(() => {
         const q = query(
         collection(dbService, "ntweets"),
@@ -26,44 +29,72 @@ const Home = ({ userObj  }) => {
         });
         }, []);
 
-    const onSubmit = async (event) => {
-    event.preventDefault();
-    try {
-    const docRef = await addDoc(collection(dbService, "ntweets"), {
-    text: ntweet,
-    createdAt: Date.now(),
-    creatorId: userObj.uid,
-    });
-    } catch (e) {
-    console.error("Error adding document: ", e);
-    }
-    setNtweet("");
-    };
-                   
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        let attachmentUrl = "";
+        if (attachment !== "") {
+            const attachmentRef = ref(storageService, `${userObj.uid}/${v4()}`);
+            const response = await uploadString(attachmentRef, attachment, "data_url");
+            attachmentUrl = await getDownloadURL(response.ref);
+            }
+            const ntweetObj = {
+                ntweet,
+                createdAt: Date.now(),
+                creatorId: userObj.uid,
+                attachmentUrl, 
+                };
+                await addDoc(collection(dbService, "ntweets"), ntweetObj);
+                setNtweet("");
+                setAttachment("");
+                };      
     const onChange = (event) => {
         const { 
             target: { value },
      } = event;
      setNtweet(value);
     };
+    const onFileChange = (event) => {
+        const {target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget: { result }
+            } = finishedEvent;
+            setAttachment(result)
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => { 
+        setAttachment("") 
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input 
-                value={ntweet} 
-                onChange={onChange} 
-                type="text" 
-                placeholder="What's on your mind?" 
-                maxLength={120}
+                    value={ntweet} 
+                    onChange={onChange} 
+                    type="text" 
+                    placeholder="What's on your mind?" 
+                    maxLength={120}
                 />
+                <input type="file" accept="image/*"  onChange={onFileChange} />
                 <input type="submit" value="Ntweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div> 
+                )}
             </form>
             <div>
             {ntweets.map((ntweet) => (
                 <Ntweet 
-                key={ntweet.id} 
-                ntweetObj={ntweet} 
-                isOwner={ntweet.creatorId === userObj.uid} />
+                    key={ntweet.id} 
+                    ntweetObj={ntweet} 
+                    isOwner={ntweet.creatorId === userObj.uid} 
+                />
             ))}
             </div>
         </div>
